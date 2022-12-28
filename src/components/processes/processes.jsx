9fcebd6d171b07fea4743dc7b9a3827a911481fa
Manish <msirvi@mcfadyen.com>
2022-12-28 14:43:00 +0530

    fixed coloumn manager and display settings

--- src/components/processes/processes.jsx
+++ src/components/processes/processes.jsx
@@ -12,7 +12,9 @@ import { NO_VALUE_FALLBACK } from '@commercetools-frontend/constants';
 import {
     usePaginationState,
     useDataTableSortingState,
+    useRowSelection
 } from '@commercetools-uikit/hooks';
+import CheckboxInput from '@commercetools-uikit/checkbox-input';
 import { BackIcon } from '@commercetools-uikit/icons';
 import Constraints from '@commercetools-uikit/constraints';
 import FlatButton from '@commercetools-uikit/flat-button';
@@ -33,7 +35,19 @@ import messages from './messages';
 // import { getErrorMessage } from '../../helpers';
 
 const Process = lazy(() => import('../process-detail'));
-
+const rows = [{
+    'id': 418,
+    'type': 'API',
+    'name': 'POST mcm/product/sync',
+    'createAt': '23-12-2022',
+    'duration': '1s',
+    'file': '',
+    'output': 'sending data ...',
+    'processStatus': 'COMPLETED',
+    'miraklStatus': 'PROCESSING',
+    'reportFile': '',
+    'action': ''
+}];
 const initialVisibleColumns = [
     { key: 'id', label: 'ID' },
     { key: 'type', label: 'Type', isSortable: true },
@@ -42,7 +56,7 @@ const initialVisibleColumns = [
     { key: 'processStatus', label: 'Process Status', isSortable: true }
 
 ];
-const hideableColumns = [
+const initialHiddenColumns = [
     { key: 'duration', label: 'Duration', isSortable: true },
     { key: 'file', label: 'File' },
     { key: 'output', label: 'Output' },
@@ -50,10 +64,7 @@ const hideableColumns = [
     { key: 'reportFile', label: 'Report File' },
     { key: 'action', label: 'Action', isSortable: true },
 ]
-const columnManager = {
-    visibleColumnKeys: initialVisibleColumns.map(({ key }) => key),
-    hideableColumns
-}
+
 const itemRenderer = (item, column, dataLocale, projectLanguages) => {
     switch (column.key) {
         // case 'roles':
@@ -84,34 +95,96 @@ const Processes = (props) => {
         projectLanguages: context.project.languages,
     }));
     const [isCondensed, setIsCondensed] = useState(false);
-    const displaySettings = { isCondensed }
-    const initialColumnsState = [...initialVisibleColumns, ...hideableColumns];
+    const [isWrappingText, setIsWrappingText] = useState(false);
+    const initialColumnsState = [...initialVisibleColumns, ...initialHiddenColumns];
     const [tableData, setTableData] = useState({
         columns: initialColumnsState,
         visibleColumnKeys: initialVisibleColumns.map(({ key }) => key),
-      });
-    //   TODO : coloumn manager
+    });
+    const withRowSelection = true;
+    const columnManager = {
+        areHiddenColumnsSearchable: true,
+        searchHiddenColumns: (searchTerm) => {
+            setTableData({
+                ...tableData,
+                columns: initialColumnsState.filter(
+                    (column) =>
+                        tableData.visibleColumnKeys.includes(column.key) ||
+                        column.label
+                            .toLocaleLowerCase()
+                            .includes(searchTerm.toLocaleLowerCase())
+                ),
+            });
+        },
+        disableColumnManager: false,
+        visibleColumnKeys: tableData.visibleColumnKeys,
+        hideableColumns: tableData.columns,
+    };
+    const displaySettings = {
+        disableDisplaySettings: false,
+        isCondensed,
+        isWrappingText
+    };
+    const UPDATE_ACTIONS = {
+        COLUMNS_UPDATE: 'columnsUpdate',
+        IS_TABLE_CONDENSED_UPDATE: 'isTableCondensedUpdate',
+        IS_TABLE_WRAPPING_TEXT_UPDATE: 'isTableWrappingTextUpdate'
+    };
+    // const displaySettings = {isCondensed};
     const tableSettingsChangeHandler = {
-        ['a'.COLUMNS_UPDATE]: (visibleColumnKeys) =>
-          setTableData({
-            ...tableData,
-            visibleColumnKeys,
-          }),
-        ['b'.IS_TABLE_CONDENSED_UPDATE]: setIsCondensed,
-      };
-    //   const { channelsPaginatedResult, error, loading } = useChannelsFetcher({
-    //     page,
-    //     perPage,
-    //     tableSorting,
-    //   });
-
-    //   if (error) {
-    //     return (
-    //       <ContentNotification type="error">
-    //         <Text.Body>{getErrorMessage(error)}</Text.Body>
-    //       </ContentNotification>
-    //     );
-    //   }
+        [UPDATE_ACTIONS.COLUMNS_UPDATE]: (visibleColumnKeys) =>
+            setTableData({
+                ...tableData,
+                visibleColumnKeys,
+            }),
+        [UPDATE_ACTIONS.IS_TABLE_CONDENSED_UPDATE]: setIsCondensed,
+        [UPDATE_ACTIONS.IS_TABLE_WRAPPING_TEXT_UPDATE]: setIsWrappingText,
+    };
+    const {
+        rows: rowsWithSelection,
+        toggleRow,
+        selectAllRows,
+        deselectAllRows,
+        getIsRowSelected,
+        getNumberOfSelectedRows,
+    } = useRowSelection('checkbox', rows);
+    const countSelectedRows = getNumberOfSelectedRows();
+    const isSelectColumnHeaderIndeterminate =
+        countSelectedRows > 0 && countSelectedRows < rowsWithSelection.length;
+    const handleSelectColumnHeaderChange =
+        countSelectedRows === 0 ? selectAllRows : deselectAllRows;
+    const mappedColumns = tableData.columns.reduce(
+        (columns, column) => ({
+            ...columns,
+            [column.key]: column,
+        }),
+        {}
+    );
+    const visibleColumns = tableData.visibleColumnKeys.map(
+        (columnKey) => mappedColumns[columnKey]
+    );
+    const columnsWithSelect = [
+        {
+            key: 'checkbox',
+            label: (
+                <CheckboxInput
+                    isIndeterminate={isSelectColumnHeaderIndeterminate}
+                    isChecked={countSelectedRows !== 0}
+                    onChange={handleSelectColumnHeaderChange}
+                />
+            ),
+            shouldIgnoreRowClick: true,
+            align: 'center',
+            renderItem: (row) => (
+                <CheckboxInput
+                    isChecked={getIsRowSelected(row.id)}
+                    onChange={() => toggleRow(row.id)}
+                />
+            ),
+            disableResizing: true,
+        },
+        ...visibleColumns,
+    ];
 
     return (
         <Spacings.Stack scale="xl">
@@ -136,30 +209,18 @@ const Processes = (props) => {
             {true ? (
                 <Spacings.Stack scale="l">
                     <DataTableManager
-                        columns={initialVisibleColumns}
+                        columns={withRowSelection ? columnsWithSelect : initialVisibleColumns}
                         columnManager={columnManager}
                         displaySettings={displaySettings}
                         onSettingsChange={(action, nextValue) => {
+
                             tableSettingsChangeHandler[action](nextValue);
-                          }}
-                    >
+                        }} >
                         <DataTable
                             isCondensed
                             columns={initialVisibleColumns}
                             disableDisplaySettings
-                            rows={[{
-                                'id': 418,
-                                'type': 'API',
-                                'name': 'POST mcm/product/sync',
-                                'createAt': '23-12-2022',
-                                'duration': '1s',
-                                'file': '',
-                                'output': 'sending data ...',
-                                'processStatus': 'COMPLETED',
-                                'miraklStatus': 'PROCESSING',
-                                'reportFile': '',
-                                'action': ''
-                            }]}
+                            rows={withRowSelection ? rowsWithSelection : rows}
                             itemRenderer={(item, column) =>
                                 itemRenderer(item, column, dataLocale, projectLanguages)
                             }
@@ -177,10 +238,10 @@ const Processes = (props) => {
                         totalItems={20}
                     />
                     <Switch>
-            {/* <SuspendedRoute path={`${match.path}/:id`}>
+                        {/* <SuspendedRoute path={`${match.path}/:id`}>
               <Process onClose={() => push(`${match.url}`)} />
             </SuspendedRoute> */}
-          </Switch>
+                    </Switch>
                 </Spacings.Stack>
             ) : null}
         </Spacings.Stack>
